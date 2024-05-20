626b54daba74011435c045a46bcf5aba2b99fe50
patrick <patrickkevinmims@gmail.com>
2024-05-20 10:59:08 -0700

    Latest

--- src/Arrays/chess_board.c
+++ src/Arrays/chess_board.c
@@ -1,14 +1,30 @@
 #include "hdr.h"
+#define CHESS_BOARD 8
 
 void chess_board()
 {
-    puts("chess_board()");
+    int COL, ROW;
+
+    char chess_board[8][8] = {
+        { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
+        { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' }
+    };
+
+    for(ROW = 0; ROW < CHESS_BOARD; ROW++) 
+    {
+        for(COL = 0; COL < CHESS_BOARD; COL++) 
+        {
+
+        }
+    }
+
 }
 
 /**
- * Write a declaration fof an 8 x 8 char array named chess_board. 
- * Include an initializer that puts the following data into the array 
- * (one character per array element).
+ * 1. Write a declaration of an 8 x 8 char array named chess_board. 
+ *
+ * 2. Include an initializer that puts the following data into the array 
+ *    (one character per array element).
  *
  * r n b q k b n r
  * p p p p p p p p
