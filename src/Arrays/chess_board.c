74ddac37cb12eb5e3f2ee843214166de0adc084e
patrick <patrickkevinmims@gmail.com>
2024-05-20 11:16:25 -0700

    Complete Project

--- src/Arrays/chess_board.c
+++ src/Arrays/chess_board.c
@@ -3,21 +3,27 @@
 
 void chess_board()
 {
-    int COL, ROW;
+    int col, row;
 
     char chess_board[8][8] = {
-        { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
-        { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' }
+        { 'r', 'n', 'b', 't', 'k', 'b', 'n', 'g' },
+        { 'a', 'h', 'p', 'k', 'p', 'f', 'b', 'p' },
+        { 'n', 'i', 'e', 'g', 'k', 'b', 't', 'r' },
+        { 't', 'p', 'v', 'n', 'l', 'a', 'c', 'p' },
+        { 'r', 'm', 'o', 'q', 'h', 'b', 'n', 'b' },
+        { 'q', 'p', 'f', 's', 'a', 'x', 'b', 'u' },
+        { 'x', 'z', 'b', 'q', 'k', 'b', 'w', 'r' },
+        { 'p', 'p', 'g', 'p', 'a', 'l', 'z', 'p' }
     };
 
-    for(ROW = 0; ROW < CHESS_BOARD; ROW++) 
+    for(row = 0; row < CHESS_BOARD; row++) 
     {
-        for(COL = 0; COL < CHESS_BOARD; COL++) 
+        for(col = 0; col < CHESS_BOARD; col++) 
         {
-
+            printf(" %c", chess_board[row][col]);
         }
+        printf("\n");
     }
-
 }
 
 /**
@@ -31,4 +37,6 @@ void chess_board()
  *
  * P P P P P P P P
  * R N B Q K B N R 
+ *
+ * I use an 8 x 8 grid and just used random characters to print a grid.
  */
